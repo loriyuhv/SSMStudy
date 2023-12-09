ce8d1a81f8335df8295fa3f49dc7a6e0450b3a94
loriyuhv <2385923602@qq.com>
2023-12-09 12:57:21 +0800

    MyBatisPlus X使用

---
+++ Y_JetbrainsPluginsUse/a_MyBatisX_plugins_use/src/main/java/com/wsw/service/UserService.java
@@ -0,0 +1,13 @@
+package com.wsw.service;
+
+import com.wsw.domain.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 23859
+* @description 针对表【user】的数据库操作Service
+* @createDate 2023-12-09 10:52:23
+*/
+public interface UserService extends IService<User> {
+
+}
