242f8790180f54dbf3c200cfa1b825d0b0539e9e
loriyuhv <2385923602@qq.com>
2024-04-09 15:30:12 +0800

    SpringMVC复习

--- BSpringMVCStudy/springmvc_01_quickstart/src/main/java/com/wsw/config/ServletContainersInitConfig.java
+++ BSpringMVCStudy/springmvc_01_quickstart/src/main/java/com/wsw/config/ServletContainersInitConfig.java
@@ -9,7 +9,8 @@ import org.springframework.web.servlet.support.AbstractDispatcherServletInitiali
  * @author loriyuhv
  * @ClassName ServletContainersInitConfig
  * @date 2024/4/9 8:01
- * @description servlet容器启动的配置，从而在里面加载spring的配置
+ * @description
+ * servlet容器启动的配置，从而在里面加载spring的配置
  */
 
 // 4. 定义一个servlet容器启动的配置类，在里面加载spring的配置
@@ -17,9 +18,12 @@ import org.springframework.web.servlet.support.AbstractDispatcherServletInitiali
 public class ServletContainersInitConfig extends AbstractDispatcherServletInitializer {
 
     // 加载SpringMVC容器配置
+    // WebApplicationContext是SpringMVC最终体现的对象，也就是说SpringMVC容器最终体现的对象是WebApplicationContext
     @Override
     protected WebApplicationContext createServletApplicationContext() {
+        // 初始化了一个空的容器对象
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        // 加载具体的配置信息
         ctx.register(SpringMvcConfig.class);
         System.out.println("Hello");
         return ctx;
