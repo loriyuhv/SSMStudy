2a1819d7726825de1ff1826810d6a7e0477ab5da
loriyuhv <2385923602@qq.com>
2024-04-13 04:27:17 +0800

    spring测试

---
+++ ASpringStudy/spring_99_exercise/spring_99_mybatis/src/main/java/com/wsw/App.java
@@ -0,0 +1,14 @@
+package com.wsw;
+
+/**
+ * @author loriyuhv
+ * @ClassName App
+ * @date 2024/4/13 4:26
+ * @description TODO
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+    }
+}
