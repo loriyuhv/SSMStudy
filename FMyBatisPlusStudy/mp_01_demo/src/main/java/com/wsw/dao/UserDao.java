713532d5f858814910f310aa04b2bf70fc2d5583
loriyuhv <2385923602@qq.com>
2025-08-16 07:19:21 +0800

    删除多余文件！！！

--- FMyBatisPlusStudy/mp_01_demo/src/main/java/com/wsw/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.wsw.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wsw.domain.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserDao extends BaseMapper<User> {
-}
