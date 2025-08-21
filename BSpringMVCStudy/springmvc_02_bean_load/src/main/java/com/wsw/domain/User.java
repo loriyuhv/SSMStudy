142508605518bdb43d854e367712cfbab1589ecb
loriyuhv <2385923602@qq.com>
2025-08-22 03:57:28 +0800

    更新版本为Spring6.2.10版本

--- BSpringMVCStudy/springmvc_02_bean_load/src/main/java/com/wsw/domain/User.java
+++ BSpringMVCStudy/springmvc_02_bean_load/src/main/java/com/wsw/domain/User.java
@@ -1,45 +1,15 @@
 package com.wsw.domain;
 
+import lombok.Data;
+
 /**
  * @author loriyuhv
  * @date 2025/8/22
  * @description
  */
+@Data
 public class User {
     private Integer id;
     private String name;
     private Integer age;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
